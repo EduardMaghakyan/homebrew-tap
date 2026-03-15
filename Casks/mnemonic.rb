538bb678e405da3eb867ec7756261c353da634e5
Eduard Maghakyan <edi.maghakyan@gmail.com>
2026-03-16 00:39:17 +0100

    mnemonic 0.1.0

--- Casks/mnemonic.rb
+++ Casks/mnemonic.rb
@@ -1,6 +1,6 @@
 cask "mnemonic" do
   version "0.1.0"
-  sha256 "PLACEHOLDER"
+  sha256 "9019ea40f031d78974c19d461b03f9f362c09c9d6f49c0b317cacd85ae25a174"
 
   url "https://github.com/EduardMaghakyan/mnemonic/releases/download/v#{version}/Mnemonic-#{version}.zip"
   name "Mnemonic"
